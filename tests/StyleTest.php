863dfbcb715a16585870b0d32774ac60d797f81b
Nicolas Mormiche <nicolas160796@gmail.com>
2021-09-30 14:15:15 +0200

    TestCase sur l'entité Style

---
+++ tests/StyleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Movie;
+use App\Entity\Style;
+use PHPUnit\Framework\TestCase;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+
+class StyleTest extends TestCase
+{
+    public function testMovieError(): void
+    {
+        $style = new Style();
+        $this->assertTrue(is_a($style->getMovies()[0], Movie::class)); // error
+    }
+    public function testMovieAssert(): void
+    {
+        $style = new Style();
+        $this->assertFalse(is_a($style->getMovies()[0], Movie::class)); // assert
+    }
+
+    public function testHaveMovieError(): void
+    {
+        $style = new Style();
+        $movie = new Movie();
+        $style->addMovie($movie);
+        $this->assertTrue(count($style->getMovies()) == 0);
+    }
+    public function testHaveMovieAssert(): void
+    {
+        $style = new Style();
+        $movie = new Movie();
+        $style->addMovie($movie);
+        $this->assertTrue(count($style->getMovies()) != 0);
+    }
+}
