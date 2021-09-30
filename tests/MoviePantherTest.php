a0c36966a167888d25e7c3d7bf99ed36c6a84020
Nicolas marcaud <nmarcau@gmail.com>
2021-09-30 16:58:58 +0200

    Fonction Panther de test du formulaire qui teste le formulaire mais pas trop de manière fictive

--- tests/MoviePantherTest.php
+++ tests/MoviePantherTest.php
@@ -13,4 +13,21 @@ class MoviePantherTest extends PantherTestCase
 
         $this->assertSelectorTextContains('h1', 'Movie index');
     }
+
+
+    public function testAddingFilm(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', 'https://localhost:8000/movie/new');
+
+        $form = $crawler->selectButton('Save')->form([
+            'movie[name]'                       => 'Yes Man',
+            'movie[description]'                => 'Un film positif',
+            'movie[release_date][date][month]'  => '5',
+            'movie[release_date][date][day]'    => '24',
+            'movie[release_date][date][year]'   => '2016',
+            'movie[styles][]'                   => '17',// Entre 17 et 20
+        ]);
+        $client->submit($form);
+    }
 }
